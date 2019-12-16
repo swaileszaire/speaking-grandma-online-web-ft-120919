0c84206b27c2fb0ff958e65677575c08bba2cfe1
Zaire  Swailes <swaileszaire@gmail.com>
2019-12-16 05:24:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ if phrase == phrase.upcase
 puts "NO, NOT SINCE 1938!"
 elsif phrase.upcase == "I LOVE YOU GRANDMA!"
 puts "I LOVE YOU TOO PUMPKIN!"
-else #{"phrase"}.downcase
+else phrase.downcase
 puts "HUH?! SPEAK UP SONNY!" 
 end
 end
