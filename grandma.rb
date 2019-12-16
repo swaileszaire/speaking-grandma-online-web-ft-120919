d6f02de6367bea75f3d48c9c01e93691b134a30c
Zaire  Swailes <swaileszaire@gmail.com>
2019-12-16 06:14:07 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase) 
 if phrase == phrase.upcase
 puts "NO, NOT SINCE 1938!"
-else phrase == phrase.downcase
-puts "HUH?! SPEAK UP SONNY!"
 elsif phrase == "I LOVE YOU GRANDMA!"
 puts "I LOVE YOU TOO PUMPKIN!"
+else phrase == phrase.downcase
+puts "HUH?! SPEAK UP SONNY!"
 end
 end
